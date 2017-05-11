735c84892152010420f8675f3dc7d4fcc117ba24
Rich <ibanez19@sbcglobal.net>
2017-05-11 14:55:06 +0000

    Enhance profile validation

--- app/common/main.js
+++ app/common/main.js
@@ -92,8 +92,13 @@ function editProfile() {
         $('#profileLocation').attr('alt') === $('#profileLocation').val()) {
             return Materialize.toast('No changes have been made.', 2000);  
         }
+    
+    //Then, ensure changes are valid
+    if ($('input').hasClass('invalid')) 
+        return Materialize.toast('Invalid information.', 2000, 'error');
         
     //If changes have been made, update the user's profile in the DB
+    progress('show');
     ajaxFunctions.ajaxRequest('PUT', 
         `/api/user/${localStorage.getItem('rv-bookclub-id')}/${$('#profileName').val()}/${$('#profileLocation').val()}`, res => {
          let result = JSON.parse(res);
@@ -102,11 +107,14 @@ function editProfile() {
          $('#navName').html(`${result.name.split(' ')[0]}`);
          $('#navImg').attr('alt', `${result.name}`);
          $('#profileName').val(`${result.name}`);
+         $('#profileName').attr('alt', `${result.name}`);
          $('#profileLocation').val(`${result.location}`);
+         $('#profileLocation').attr('alt', `${result.location}`);
          
          //Close modal and notify the user
          $('#profileModal').modal('close');
-         Materialize.toast('Your profile has been updated!', 3000);
+         Materialize.toast('Your profile has been updated!', 2000);
+         progress('hide');
     });
     
 }
@@ -117,6 +125,7 @@ function resetProfile() {
     //Restore original profile values
     $('#profileName').val($('#profileName').attr('alt'));
     $('#profileLocation').val($('#profileLocation').attr('alt'));
+    $('input').removeClass('invalid');
     
     //Close modal and notify the user
     $('#profileModal').modal('close');
